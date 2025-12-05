19e8586e040b748cb0e5c0dd9ab2589d9d69d189
İlhan Can Tuğcu <ilhancantugcu@hotmail.com>
2025-12-05 17:10:45 +0300

    Update

--- CineLog.Core/Entities/UserMovie.cs
+++ CineLog.Core/Entities/UserMovie.cs
@@ -16,9 +16,9 @@ namespace CineLog.Core.Entities
         public int MovieId { get; set; }
         public Movie Movie { get; set; }
 
-        // 3. EKSTRA BİLGİLER (Payload)
+        // 3. EKSTRA BİLGİLER
         public DateTime AddedDate { get; set; } = DateTime.Now;
-        public bool IsWatched { get; set; } = false; // "İzledim" işaretlemek için
-        public int? Rating { get; set; } // Kullanıcının vereceği özel puan (1-10)
+        public bool IsWatched { get; set; } = false; 
+        public int? Rating { get; set; } // Kullanıcının vereceği puan (1-10)
     }
 }
